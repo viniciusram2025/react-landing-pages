18f3b00a78d06c86cc1f65d8123666d37729a034
Vinicius de Paula <vinicius.ram2025@gmail.com>
2022-12-12 17:41:30 -0300

    snipets do vscode

---
+++ src/components/Dummy/Dummy.test.jsx
@@ -0,0 +1,10 @@
+import { screen } from '@testing-library/react';
+import { Dummy } from '.';
+import { renderTheme } from '../../styles/render-theme';
+
+describe('<Dummy />', () => {
+  it('should render', () => {
+    renderTheme(<Dummy>Children</Dummy>);
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+  });
+});
---
+++ src/components/Dummy/styles.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+
+    ${({ theme }) => css``}
+`;
