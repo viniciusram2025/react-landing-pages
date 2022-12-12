ed253cac2c9fa2e568228346454305cf92694911
Vinicius de Paula <vinicius.ram2025@gmail.com>
2022-12-12 15:13:56 -0300

    Mais edições de estilos e Inclusão dos testes do componente Heading

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
 import Home from './templates/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,20 +1,10 @@
-import { screen } from '@testing-library/react';
+//import { screen } from '@testing-library/react';
 import Home from '.';
 import { renderTheme } from '../../styles/render-theme';
-import { theme } from '../../styles/theme';
+//import { theme } from '../../styles/theme';
 
-test('renders learn react link', () => {
-  const { debug } = renderTheme(<Home />);
-
-  const headingContainer = screen.getByRole('heading', {
-    name: 'Hello',
-  }).parentElement;
-
-  //duas formas de fazer o mesmo teste
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.mainBg,
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
   });
-  //expect(headingContainer).toHaveStyleRule('background', 'red');
-
-  expect(headingContainer).toMatchSnapshot();
 });
