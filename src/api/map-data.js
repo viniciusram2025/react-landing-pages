6840795d4a8087376b9228e58271259237978ff6
Vinicius de Paula <vinicius.ram2025@gmail.com>
2022-12-21 18:38:42 -0300

    criando api e testes da api

--- src/api/map-data.js
+++ src/api/map-data.js
@@ -1,9 +1,26 @@
-export const mapData = () => {
-  return {
-    footerHtml: '',
-    slug: '',
-    title: '',
-    sections: [],
-    menu: {},
-  };
+import { mapSections } from './map-sections';
+import { mapMenu } from './map-menu';
+
+export const mapData = (pagesData = [{}]) => {
+  return pagesData.map((data) => {
+    const {
+      footer_text: footerHtml = '',
+      slug = '',
+      title = '',
+      sections = [],
+      menu = {},
+    } = data;
+
+    return {
+      footerHtml,
+      slug,
+      title,
+      sections: mapSections(sections),
+      menu: mapMenu(menu),
+    };
+  });
 };
+
+//const { inspect } = require('util');
+//const dados = require('./dados.json');
+//console.log(inspect(mapData(dados), null, null, true));
--- src/api/map-data.test.js
+++ src/api/map-data.test.js
@@ -2,11 +2,22 @@ import { mapData } from './map-data';
 
 describe('map-data', () => {
   it('should map data even it there is no data', () => {
-    const pagesData = mapData();
+    const pagesData = mapData()[0];
     expect(pagesData.footerHtml).toBe('');
     expect(pagesData.slug).toBe('');
     expect(pagesData.title).toBe('');
-    expect(pagesData.sections).toEqual([]);
-    expect(pagesData.menu).toEqual({});
+  });
+
+  it('should map data if there are data', () => {
+    const pagesData = mapData([
+      {
+        footer_text: '<p>Hey</p>',
+        slug: 'slug',
+        title: 'title',
+      },
+    ])[0];
+    expect(pagesData.footerHtml).toBe('<p>Hey</p>');
+    expect(pagesData.slug).toBe('slug');
+    expect(pagesData.title).toBe('title');
   });
 });
